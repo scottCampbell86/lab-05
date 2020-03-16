6cb2b1fbaaa87356b125951b02ae005a5481439e
Scott Campbell <stc9486@gmail.com>
2020-03-15 19:41:34 -0700

    3

--- __test__/routes.test.js
+++ __test__/routes.test.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const request = require('supertest');
 const app = require('../lib/app');
+const Note = require('../models/Notes.js');
 
 describe('note routes', () => {
     beforeAll(() => {
@@ -33,5 +34,47 @@ describe('note routes', () => {
                     __v: 0
                 });
             });
+    });
+
+    it('gets a new note with a GET', () => {
+        return Note.create({
+            title: 'My Note',
+            body: 'My cool note',
+            category: 'animal'
+        })
+            .then(note => {
+                return request(app)
+                    .get(`/api/v1/notes/${note.id}`);
+            }) 
+            .then(res => {
+                expect(res.body).toEqual({
+                    _id: expect.any(String),
+                    title: 'My Note',
+                    body: 'My cool note',
+                    category: 'animal',
+                    __v: 0
+                });
+            });
+    });
+    
+    it ('gets all notes via GET', () => {
+        const notes = [
+            { title: 'Note 1', body: 'Note 1', category: 'animal' },
+            { title: 'Note 2', body: 'Note 2', category: 'animal' },
+            { title: 'Note 3', body: 'Note 3', category: 'animal' }
+        ];
+        return Note.create(notes)
+            .then(() => {
+                return request(app)
+                    .get(`/api/v1/notes`);
+            })
+            .then(res => {
+                notes.forEach(note => {
+                    expect(res.body).toContainEqual({ ...note,
+                         _id: expect.any(String),
+                         __v:0
+                    });
+            });
+        });
     });
 });
