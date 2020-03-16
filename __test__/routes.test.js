98e1bc4f4f904fa77ccc183396d164879c0fc666
Scott Campbell <stc9486@gmail.com>
2020-03-15 21:07:06 -0700

    4/6

--- __test__/routes.test.js
+++ __test__/routes.test.js
@@ -77,4 +77,74 @@ describe('note routes', () => {
             });
         });
     });
+
+    it ('gets category notes via GET', () => {
+        const notes = [
+            { title: 'Note 1', body: 'Note 1', category: 'animal' },
+            { title: 'Note 2', body: 'Note 2', category: 'other' },
+            { title: 'Note 3', body: 'Note 3', category: 'study' }
+        ];
+        return Note.create(notes)
+            .then(() => {
+                return request(app)
+                    .get(`/api/v1/notes/category/animal`);
+            })
+            .then(res => {
+                const onlyAnimalNotes = notes.filter(note => note.category === 'animal');
+                expect(res.body).toHaveLength(onlyAnimalNotes.length);
+                onlyAnimalNotes
+                    .filter(note => note.category === 'animal') 
+                    .forEach(note => {
+                        expect(res.body).toContainEqual({
+                                 ...note,
+                                _id: expect.any(String),
+                                __v:0
+                    })
+                });
+            });
+    });
+
+    it('updates a note by id via patch', () => {
+        return Note.create({
+            title: 'Me nite',
+            body: 'this is a note',
+            category: 'animal'
+        })
+        .then(note => {
+            return request(app)
+                .patch(`/api/v1/notes/${note.id}`)
+                .send({title: 'My note'});
+        })
+        .then(res => {
+            expect(res.body).toEqual({
+                _id: expect.any(String),
+                title: 'My note',
+                body: 'this is a note',
+                category: 'animal',
+                __v: 0
+            });
+        });
+    });
+
+    it('detes a not via Delete', () => {
+        return Note.create({
+            title: 'Note 1',
+            body: 'my body',
+            cateorgy: 'animal'
+        })
+        .then(note => {
+            return request(app)
+                .delete(`/api/v1/notes/${note.id}`)
+        })
+        .then(res => {
+            expect(res.body).toEqual ({
+                _id: expect.any(String),
+                title: 'Note 1',
+                body: 'my body',
+                cateorgy: 'animal',
+                __v: 0
+            })
+        })
+    })
+    
 });
